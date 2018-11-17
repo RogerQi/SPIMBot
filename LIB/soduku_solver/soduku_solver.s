8501c7abc8ab322aa3dc69ac28557b450fd12b6a
Roger Q <twofs0.5@gmail.com>
2018-11-17 15:11:51 -0600

    update soduku functions

--- LIB/soduku_solver/soduku_solver.s
+++ LIB/soduku_solver/soduku_solver.s
@@ -1,2 +1,19 @@
 .text
+# bool solve(unsigned short board[GRID_SQUARED][GRID_SQUARED]) {
+#     bool changed;
+#     do {
+#         changed = rule1(board);
+#         changed |= rule2(board);
+#     } while (changed);
+#
+#     if (board_done(board)) {
+#         printf("SUCCESS!\n");
+#         print_board(board);
+#     } else {
+#         printf("FAILED!\n");
+#         print_board_verbose(board);
+#     }
+#     printf("\n");
+# }
+
 # .globl soduku_solver
