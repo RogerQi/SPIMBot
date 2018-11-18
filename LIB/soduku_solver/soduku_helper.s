d295c7c184b2d2a59a01dc5239e88e60683887e5
Roger Q <twofs0.5@gmail.com>
2018-11-17 20:16:37 -0600

    more house keeping stuffs

--- LIB/soduku_solver/soduku_helper.s
+++ LIB/soduku_solver/soduku_helper.s
@@ -1,146 +1,146 @@
 .globl get_square_begin
 get_square_begin:
-	# round down to the nearest multiple of 4
-	div	$v0, $a0, 4
-	mul	$v0, $v0, 4
-	jr	$ra
+    # round down to the nearest multiple of 4
+    div	$v0, $a0, 4
+    mul	$v0, $v0, 4
+    jr	$ra
 
 
 # UNTIL THE SOLUTIONS ARE RELEASED, YOU SHOULD COPY OVER YOUR VERSION FROM LAB 7
 # (feel free to copy over the solution afterwards)
 .globl has_single_bit_set
 has_single_bit_set:
-	li	$v0, 0
-	beq $a0, $zero, has_single_bit_set_end #(if value == 0) return 0;
+    li	$v0, 0
+    beq $a0, $zero, has_single_bit_set_end #(if value == 0) return 0;
 
-	li $t1, 1
-	sub $t1, $a0, $t1
-	and $t1, $t1, $a0
-	bne $t1, $zero, has_single_bit_set_end
-	j has_single_bit_set_ret_one
+    li $t1, 1
+    sub $t1, $a0, $t1
+    and $t1, $t1, $a0
+    bne $t1, $zero, has_single_bit_set_end
+    j has_single_bit_set_ret_one
 
 has_single_bit_set_ret_one:
-	li $v0, 1
-	j has_single_bit_set_end
+    li $v0, 1
+    j has_single_bit_set_end
 
 has_single_bit_set_end:
-	jr	$ra
+jr	$ra
 
 
 # UNTIL THE SOLUTIONS ARE RELEASED, YOU SHOULD COPY OVER YOUR VERSION FROM LAB 7
 # (feel free to copy over the solution afterwards)
 get_lowest_set_bit:
-	li $t0, 0
-	j get_lowest_set_bit_loop
+    li $t0, 0
+    j get_lowest_set_bit_loop
 
 get_lowest_set_bit_end:
-	li $v0, 0
-	jr $ra
+    li $v0, 0
+    jr $ra
 
 get_lowest_set_bit_ret_i:
-	move $v0, $t0
-	jr $ra
+    move $v0, $t0
+    jr $ra
 
 get_lowest_set_bit_loop:
-	sltiu $t1, $t0, 16 #t0 is i
-	beq $t1, $zero, get_lowest_set_bit_end #end if it's 0
-	li $t1, 1
-	sll $t1, $t1, $t0
-	and $t1, $t1, $a0
-	bne $t1, $zero, get_lowest_set_bit_ret_i
-	addi $t0, $t0, 1
-	j get_lowest_set_bit_loop
+    sltiu $t1, $t0, 16 #t0 is i
+    beq $t1, $zero, get_lowest_set_bit_end #end if it's 0
+    li $t1, 1
+    sll $t1, $t1, $t0
+    and $t1, $t1, $a0
+    bne $t1, $zero, get_lowest_set_bit_ret_i
+    addi $t0, $t0, 1
+    j get_lowest_set_bit_loop
 
 # UNTIL THE SOLUTIONS ARE RELEASED, YOU SHOULD COPY OVER YOUR VERSION FROM LAB 7
 # (feel free to copy over the solution afterwards)
 .globl print_board
 print_board:
-	#Start Saving S register...
-	sub $sp, $sp, 24
-	sw $s0, 0($sp)
-	sw $s1, 4($sp)
-	sw $s2, 8($sp)
-	sw $s3, 12($sp)
-	sw $s4, 16($sp)
-	sw $ra, 20($sp)
-	#End Saving Sx
-	li $t0, 0 #t0 = i
-	j print_board_i_loop
+    #Start Saving S register...
+    sub $sp, $sp, 24
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+    sw $s3, 12($sp)
+    sw $s4, 16($sp)
+    sw $ra, 20($sp)
+    #End Saving Sx
+    li $t0, 0 #t0 = i
+    j print_board_i_loop
 
 print_board_i_loop:
-	slt $t7, $t0, 16
-	bne $t7, 1, print_board_end
-	li $t1, 0 #t1 = j
-	j print_board_j_loop
+    slt $t7, $t0, 16
+    bne $t7, 1, print_board_end
+    li $t1, 0 #t1 = j
+    j print_board_j_loop
 
 print_board_i_loop_end:
-	move $s0, $a0
-	addi $t0, $t0, 1
-	li $v0, 11
-	li $a0, '\n'
-	syscall #putchar \n
-	move $a0, $s0
-	j print_board_i_loop
+    move $s0, $a0
+    addi $t0, $t0, 1
+    li $v0, 11
+    li $a0, '\n'
+    syscall #putchar \n
+    move $a0, $s0
+    j print_board_i_loop
 
 print_board_j_loop:
-	slt $t7, $t1, 16
-	bne $t7, 1, print_board_i_loop_end
-	mul $t2, $t0, 32 #i offset
-	mul $t7, $t1, 2 #j offset
-	add $t7, $t2, $t7 #ele offset
-	add $t2, $a0, $t7 #true mem addr
-	lhu $t2, 0($t2) #t2: int value = board[i][j]
-	li $t3, '*' #t3: char c = '*'
-	move $s0, $a0
-	move $s1, $t0
-	move $s2, $t1
-	move $s3, $t2
-	move $s4, $t3
-	move $a0, $t2
-	jal has_single_bit_set
-	move $a0, $s0
-	move $t0, $s1
-	move $t1, $s2
-	move $t2, $s3
-	move $t3, $s4
-	#if bit is not set; end loop.
-	beq $v0, 0, print_board_j_loop_end
-	#suppose bit is set; change c and end loop
-	move $s0, $a0
-	move $s1, $t0
-	move $s2, $t1
-	move $s3, $t2
-	move $s4, $t3
-	move $a0, $t2 #set input param to be value
-	jal get_lowest_set_bit
-	move $a0, $s0
-	move $t0, $s1
-	move $t1, $s2
-	move $t2, $s3
-	move $t3, $s4
-	addi $t4, $v0, 1 #t4: int num = get_lowest_set_bit(value) + 1;
-	la $t7, symbollist #t7 = &symbollist
-	add $t7, $t7, $t4
-	lbu $t3, 0($t7) #c = symbollist[num];
-	j print_board_j_loop_end
+    slt $t7, $t1, 16
+    bne $t7, 1, print_board_i_loop_end
+    mul $t2, $t0, 32 #i offset
+    mul $t7, $t1, 2 #j offset
+    add $t7, $t2, $t7 #ele offset
+    add $t2, $a0, $t7 #true mem addr
+    lhu $t2, 0($t2) #t2: int value = board[i][j]
+    li $t3, '*' #t3: char c = '*'
+    move $s0, $a0
+    move $s1, $t0
+    move $s2, $t1
+    move $s3, $t2
+    move $s4, $t3
+    move $a0, $t2
+    jal has_single_bit_set
+    move $a0, $s0
+    move $t0, $s1
+    move $t1, $s2
+    move $t2, $s3
+    move $t3, $s4
+    #if bit is not set; end loop.
+    beq $v0, 0, print_board_j_loop_end
+    #suppose bit is set; change c and end loop
+    move $s0, $a0
+    move $s1, $t0
+    move $s2, $t1
+    move $s3, $t2
+    move $s4, $t3
+    move $a0, $t2 #set input param to be value
+    jal get_lowest_set_bit
+    move $a0, $s0
+    move $t0, $s1
+    move $t1, $s2
+    move $t2, $s3
+    move $t3, $s4
+    addi $t4, $v0, 1 #t4: int num = get_lowest_set_bit(value) + 1;
+    la $t7, symbollist #t7 = &symbollist
+    add $t7, $t7, $t4
+    lbu $t3, 0($t7) #c = symbollist[num];
+    j print_board_j_loop_end
 
 print_board_j_loop_end:
-	li $v0, 11
-	move $s0, $a0
-	move $a0, $t3
-	syscall #putchar c
-	addi $t1, $t1, 1
-	move $a0, $s0
-	j print_board_j_loop
+    li $v0, 11
+    move $s0, $a0
+    move $a0, $t3
+    syscall #putchar c
+    addi $t1, $t1, 1
+    move $a0, $s0
+    j print_board_j_loop
 
 print_board_end:
-	#restore S register and SP...
-	lw $s0, 0($sp)
-	lw $s1, 4($sp)
-	lw $s2, 8($sp)
-	lw $s3, 12($sp)
-	lw $s4, 16($sp)
-	lw $ra, 20($sp)
-	add $sp, $sp, 24
-	#End restoring S/SP
-	jr $ra
+    #restore S register and SP...
+    lw $s0, 0($sp)
+    lw $s1, 4($sp)
+    lw $s2, 8($sp)
+    lw $s3, 12($sp)
+    lw $s4, 16($sp)
+    lw $ra, 20($sp)
+    add $sp, $sp, 24
+    #End restoring S/SP
+    jr $ra
--- make.py
+++ make.py
@@ -7,6 +7,8 @@ dependency_dir = ["./LIB/"]
 assembled_file_path = "./spimbot.s"
 qtspimbot_command_line = "QtSpimbot -file spimbot.s -mapseed 233"
 
+seperate_str = "\r\n\r\n#/***************{0}***************/\r\n"
+
 def print_help():
     print("Example Usage: python {0} test_puzzle_solver".format(sys.argv[0]))
     print("The above command would acquire main/test_puzzle_solver.s and assemble it with BSP/LIB to make a comprehensive file for submission")
@@ -31,13 +33,16 @@ def main():
     with open(main_file_path) as f:
         assembled_file_content += f.read()
 
-    assembled_file_content += "\r\n\r\n#/***************End Main File***************/"
-    assembled_file_content += "\r\n\r\n#/***************Start Lib File***************/\r\n"
+    assembled_file_content += seperate_str.format("End Main File")
+    assembled_file_content += seperate_str.format("Start Lib File")
 
     for lib_file_path in visible_libs:
-        print("Processing: {0}".format(lib_file_path))
+        cur_file_name = lib_file_path[len(lib_file_path) - lib_file_path[::-1].find("/"):]
+        print("Processing: {0}".format(cur_file_name))
+        assembled_file_content += seperate_str.format("Start " + cur_file_name)
         with open(lib_file_path) as f:
             assembled_file_content += f.read()
+        assembled_file_content += seperate_str.format("End " + cur_file_name)
 
     with open(assembled_file_path, "w") as f:
         f.write(assembled_file_content)
