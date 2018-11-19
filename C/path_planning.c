825cedb09e4fb521bd0d92fc6c6f092599e35138
Roger Q <twofs0.5@gmail.com>
2018-11-19 14:57:31 -0600

    update pp function sign again

--- C/path_planning.c
+++ C/path_planning.c
@@ -1,1 +1,10 @@
 #include "path_planning.h"
+
+//note that when accessing, for example, current_map->map[i][j]
+//is equivalent to (word) current_map[i][j]
+void bfs(maze_map* current_map, unsigned int* target_point) {
+    int target_x = target_point[0];
+    int target_y = target_point[1];
+
+
+}
--- C/path_planning.h
+++ C/path_planning.h
@@ -3,16 +3,20 @@
 
 #include "my_types.h"
 
+static unsigned int command_buffer[1000];
+
 //obsolete
 //0 - clear; 1 - barrier; 2 - unknown
 //unsigned int current_maze[61][61];
 
 //path planning function signature
 /**
+ * @brief update command buffer
  * @param current_map: pointer to maze_map struct (processed and all unexplored cells are stored as open path)
- * @param command_buffer: pointer to command buffer (e.g. EAST SOUTH STOP), which is terminated with a STOP
  * @param target_point: an int array of LENGTH 2 (x, y) in cell coordinate (spimbot_x / 10, spimbot_y / 10)
  */
-typedef void (*path_planning)(maze_map* current_map, int* command_buffer, int* target_point);
+typedef void (*path_planning)(maze_map* current_map, unsigned int* target_point);
+
+void bfs(maze_map* current_map, unsigned int* target_point);
 
 #endif
